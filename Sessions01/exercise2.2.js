55b7033bb2259f0d3191003c5c2947b79fd1730c
nhontd93 <94519895+nhontd93@users.noreply.github.com>
2021-11-17 15:45:02 +0700

    Delete Sessions01 directory

--- Sessions01/exercise2.2.js
+++
@@ -1,9 +0,0 @@
-function randomMinMax(min,max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-console.log('1,99: ',randomMinMax(1,99));
-console.log('2,4: ',randomMinMax(2,4));
-console.log('4,142: ',randomMinMax(4,142));
-console.log('0,56: ',randomMinMax(0,56));
-console.log('-5,6: ',randomMinMax(-5,6));
--- Sessions01/exercise2.3.js
+++
@@ -1,5 +0,0 @@
-function randomArray(arr) {
-    return arr[Math.floor(Math.random() * (arr.length))];
-}
-
-console.log('mot', 'hai', 'ba', '4', 'true, ',randomArray(['mot', 'hai', 'ba', 4, true]));
